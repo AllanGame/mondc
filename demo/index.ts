5fe19fbb92dd57b9335c182f6fd6d0095c64f4a8
AllanGame <allangameyt28@gmail.com>
2021-07-05 12:46:39 -0500

    Added a better way to register actionables

--- demo/index.ts
+++ demo/index.ts
@@ -1,3 +1,28 @@
-import { CommandManager } from 'mondc';
+import { Client, Message } from 'discord.js';
+import { Command, CommandManager } from 'mondc';
 
 const commandManager = new CommandManager();
+
+class ExampleCommand implements Command {
+    name: string;
+    aliases: string[];
+    permissions: string[];
+    client: Client;
+    execute(message: Message): void {
+        throw new Error('Method not implemented.');
+    }
+    
+}
+
+class ExampleCommand2 implements Command {
+    name: string;
+    aliases: string[];
+    permissions: string[];
+    client: Client;
+    execute(message: Message): void {
+        throw new Error('Method not implemented.');
+    }
+    
+}
+
+commandManager.register(new ExampleCommand(), new ExampleCommand2());
